643e3233bef6904bae30d7e71639811c2c4ffe5a
rsendres <ricardoendres@gmail.com>
2022-06-14 14:47:04 -0300

    adicionando alguns comentarios

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
-let canvas= document.getElementById("snake");
-let context= canvas.getContext("2d");
-let box= 32;
-let snake= [];
+let canvas = document.getElementById("snake");
+/*getContext ("2d") desenha, ele renderiza o desenho que vai acontecer dentro do canvas como 2d ele vai passar a tratar o arquivo como plano de duas dimensoes
+*/
+let context = canvas.getContext ("2d");
+/*aqui determinamos que a variavel box tera o valor 32*/
+let box = 32;
+let snake = [];
 snake[0] = {
     x: 8 * box,
-    y: 8 * box
+    y: 8 * box 
 }
+
 let direction = "right";
 let food = {
     x: Math.floor(Math.random() * 15 + 1) * box,
@@ -13,50 +17,51 @@ let food = {
 }
 
 function criarBG() {
-    context.fillStyle= "lightgreen";
-    context.fillRect(0, 0, 16 * box, 16 * box);
-    }
-
-    function criarCobrinha(){
-        for(i=0; i < snake.length; i++){
-            context.fillStyle="green";
-            context.fillRect(snake[i].x, snake[i].y, box, box);
-          }
+    /*ele esta atribuindo alguma coisa no formato css.*/
+    context.fillStyle = "lightgreen";
+    /*o fillRect eh o retangulo, dentro dele tera o jogo*/
+    context.fillRect (0, 0, 16 * box, 16 * box);
 }
 
-function drawFood(){
+function criarCobrinha(){
+    for(i = 0; i < snake.length; i++){
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
+function drawFood () {
     context.fillStyle = "red";
     context.fillRect(food.x, food.y, box, box);
 }
 
-document.addEventListener('keydown', update);
+document.addEventListener("keydown", update);
 
 function update (event){
-    if(event.keyCode == 37 && direction != "right") direction= "left";
-    if(event.keyCode == 38 && direction != "down") direction= "up";
-    if(event.keyCode == 39 && direction != "left") direction= "right";
-    if(event.keyCode == 40 && direction != "up") direction= "down";
-}
-
-function iniciarJogo(){
-
-if(snake[0].x > 15 * box && direction == "rigth") snake[0].x = 0;
-if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
-if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
-if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
-
-
-for(i = 1; i < snake.length; i++){
-if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
-    clearInterval(jogo);
-    alert("Game Over :(");
-}
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
 
 }
-    criarBG( );
-    criarCobrinha( );
-    drawFood( );
 
+
+function iniciarJogo () {
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0        && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0        && direction == "up") snake[0].y = 16 * box;
+
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Game Over :(');
+        }
+    }
+
+    criarBG();
+    criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
@@ -67,11 +72,10 @@ if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
     if(direction == "down") snakeY += box;
 
     if(snakeX != food.x || snakeY != food.y){
-        snake.pop();
-    }
-    else{ food.x = Math.floor(Math.random() * 15 + 1) * box,
-          food.y = Math.floor(Math.random() * 15 + 1) * box
-
+        snake.pop(); 
+    }else{
+        food.x = Math.floor(Math.random() * 15 +1) * box;
+        food.y = Math.floor(Math.random() * 15 +1) * box;
     }
 
     let newHead = {
@@ -79,8 +83,7 @@ if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
         y: snakeY
     }
 
-    snake.unshift(newHead);
-
+    snake.unshift(newHead)
 
 }
 
